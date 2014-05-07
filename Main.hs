1ad972bc6438e6eac419b072ad5382f2345b8c2c
Curtis Gagliardi <cgag@riseup.net>
2014-05-07 00:22:22 -0700

    Basic TTY manipulation, needed to set NoBuffering

--- Main.hs
+++ Main.hs
@@ -1,17 +1,33 @@
+import Control.Monad
+
 import System.IO
 import System.Process
+import System.Console.ANSI
 
 main :: IO ()
 main = do
   tty <- openFile "/dev/tty" ReadWriteMode
-  let procSpec = shell "stty raw -echo cbreak"
+  hSetBuffering tty NoBuffering
+
+  {-let procSpec = shell "stty raw -echo cbreak"-}
+  let procSpec = shell "stty -echo cbreak"
   _ <- createProcess procSpec
+
+  hSetSGR tty [SetColor Background Vivid White]
+  hHideCursor tty
+
+  {-inputFiles <- hGetContents stdin -}
+  {-putStrLn inputFiles-}
+
+  {-replicateM_ 10 $ hPutChar tty '\n'-}
+  {-hCursorUp tty 5-}
   loop tty
   where
     loop tty = do 
         x <- hGetChar tty 
-        hPutChar tty x
-        hPutStr tty "hello\n"
+        hPutChar tty x 
+        {-hCursorForward tty 1-}
+        hCursorDown tty 1
         if x == 'x' then
           return ()
         else
