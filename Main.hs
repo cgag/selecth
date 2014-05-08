2e4859a303ca4ce78254c44d6ddbc0223e5f0d11
Curtis Gagliardi <cgag@riseup.net>
2014-05-08 00:53:07 -0700

    Glory glory hallelujah successfully handling piped input.

--- Main.hs
+++ Main.hs
@@ -2,33 +2,59 @@ import Control.Monad
 
 import System.IO
 import System.Process
+import System.Exit
 import System.Console.ANSI
+
+configureTty :: Handle -> IO ()
+configureTty tty = do
+    -- LineBuffering by default requires hitting enter to see anything
+    hSetBuffering tty NoBuffering 
+
+    -- this will close the stdin handle, we need to use a second handle to 
+    -- /dev/tty
+    ttyTmpHandle <- openFile "/dev/tty" ReadMode
+    let procSpec = (shell "stty -echo cbreak") { 
+                          std_in  = UseHandle ttyTmpHandle
+                        , std_out = CreatePipe
+                        , std_err = CreatePipe
+                    }
+  
+    (_, _, _, processHandle) <- createProcess procSpec
+    exitCode <- waitForProcess processHandle 
+    case exitCode of
+        (ExitFailure code) -> error $ "Failed to configure tty (Exit code: "
+                                          ++ show code ++ ")"
+        ExitSuccess -> return ()
+
+{-restoreTTY :: IO ()-}
 
 main :: IO ()
 main = do
+  -- tty <- openFile "/dev/tty" ReadWriteMode
   tty <- openFile "/dev/tty" ReadWriteMode
-  hSetBuffering tty NoBuffering
-
-  {-let procSpec = shell "stty raw -echo cbreak"-}
-  let procSpec = shell "stty -echo cbreak"
-  _ <- createProcess procSpec
+  configureTty tty
 
   hSetSGR tty [SetColor Background Vivid White]
-  hHideCursor tty
+  {-hHideCursor tty-}
 
-  {-inputFiles <- hGetContents stdin -}
-  {-putStrLn inputFiles-}
+  putStrLn "hello"
+
+  inputFiles <- getContents 
+  putStrLn inputFiles
 
   {-replicateM_ 10 $ hPutChar tty '\n'-}
   {-hCursorUp tty 5-}
+
   loop tty
   where
     loop tty = do 
         x <- hGetChar tty 
-        hPutChar tty x 
-        {-hCursorForward tty 1-}
+        hCursorForward tty 1
         hCursorDown tty 1
-        if x == 'x' then
+        hPutChar tty x 
+        if x == 'x' 
+        then do
+          hClose tty
           return ()
         else
           loop tty
