2e7265e4d353d27df9561618513bac314a1204ce
Curtis Gagliardi <cgag@riseup.net>
2014-05-15 15:58:22 -0700

    minMatchLength scoring, ignore unhandled control characters.

--- Main.hs
+++ Main.hs
@@ -5,9 +5,10 @@ import Control.Monad
 
 {-import Control.Concurrent (threadDelay)-}
 
+import Data.Char
 import Data.List
-import qualified Data.Map as M
 import Data.Maybe
+import qualified Data.Map as M
 import Data.Monoid
 import Data.Function (on)
 
@@ -23,46 +24,59 @@ import Score
 
 {-TODO: implement withTty that handles restoring tty state-}
 
-data KeyPress = CtrlC | Enter | Backspace | PlainChar Char
+data KeyPress = CtrlC | Enter | Invisible | Backspace | PlainChar Char
 
 data Search = Search  
     { query :: String
     , choices :: [String]
     , selection :: Int } deriving Show
 
+data RenderedSearch = RenderedSearch 
+    {
+      queryString :: String
+    , renderedLines :: [String]
+    }
+
 data Choice = Choice 
     { finalMatches :: [String]
     , matchIndex :: Int
     }
 
-data Action = NewSearch Search | MakeChoice Choice | Abort
+data Action = NewSearch Search | MakeChoice Choice | Abort | Ignore
+
 
 specialChars :: M.Map Char KeyPress
 specialChars = M.fromList [ ('\ETX', CtrlC)
                           , ('\r',   Enter)
-                          , ('\DEL',   Backspace)]
+                          , ('\DEL', Backspace)]
 
 charToKeypress :: Char -> KeyPress
-charToKeypress c = fromMaybe (PlainChar c) (M.lookup c specialChars)
+charToKeypress c = fromMaybe (if isPrint c then PlainChar c else Invisible) 
+                             (M.lookup c specialChars)
 
 matches :: String -> [String] -> [String]
-matches qry chs = take choicesToShow $ 
-                  map fst $ 
-                  filter (\(_,cScore) -> cScore > 0) $
-                  sortBy (flip compare `on` snd) scoredChoices
+matches qry chs = take choicesToShow 
+                  $ map fst 
+                  $ filter (\(_,cScore) -> cScore > 0) 
+                  $ sortBy (flip compare `on` snd) scoredChoices
   where 
       scoredChoices =  map (\choice -> (choice, score qry choice)) chs
                        {-`using` parListChunk 10000 rdeepseq-}
 
-{-TODO: this shiould return [string] and then writelines should be called on it-}
-render :: Search -> (String, [String])
+-- Seach -> (QueryString, renderedLines)
+render :: Search -> RenderedSearch
 render (Search {query=q, choices=cs}) = 
     let queryLine = "> " <> q
         matchLines = pad (take choicesToShow $ matches q cs)  
-    in (queryLine, queryLine : matchLines)
+    in RenderedSearch queryLine $ queryLine : matchLines
   where 
     pad xs = xs ++ replicate (choicesToShow - length xs) " "
 
+draw :: Handle -> RenderedSearch -> IO ()
+draw tty rendered = do
+    withCursorHidden tty $ 
+        writeLines tty (renderedLines rendered)
+    hSetCursorColumn tty $ length $ queryString rendered
 
 -- TODO: Handle exceptions, ensure tty gets restored to default settings
 -- TODO: Ensure handle to tty is closed?  See what GB ensures.
@@ -83,6 +97,7 @@ writeSelection tty choice = do
 abort :: Handle -> IO ()
 abort tty = do
     hCursorDown tty 1 
+    saneTty -- duplicated in writeSelection
     putStrLn "Quit :(" 
     exitSuccess
 
@@ -91,11 +106,12 @@ handleInput inputChar search =
     case charToKeypress inputChar of
         CtrlC -> Abort
         Enter -> MakeChoice Choice { 
-                                  finalMatches = matches (query search) (choices search)
-                                , matchIndex = selection search
-                              } 
+                              finalMatches = matches (query search) (choices search)
+                            , matchIndex = selection search
+                            } 
         Backspace   -> NewSearch search { query = dropLast $ query search }
         PlainChar c -> NewSearch search { query = query search ++ [c] }
+        Invisible -> Ignore
 
 choicesToShow :: Int
 choicesToShow = 10
@@ -107,10 +123,11 @@ main = do
     initialChoices <- liftM lines getContents
 
     let initSearch = Search { query="", choices=initialChoices, selection=0 }
+    draw tty $ render initSearch
 
+    -- maybe this should return Abort | Quit?, ExitSuccess + sanetty duped
     _ <- eventLoop tty initSearch 
 
-    saneTty -- duplicated in writeSelection
     hClose tty 
   where
     eventLoop tty search = do
@@ -118,8 +135,7 @@ main = do
       case handleInput x search of
           Abort -> abort tty
           MakeChoice c -> writeSelection tty c
+          Ignore -> eventLoop tty search
           NewSearch newSearch -> do
-              let (queryLine, renderedLines) = render newSearch
-              withCursorHidden tty $ writeLines tty renderedLines
-              hSetCursorColumn tty (length queryLine)
+              draw tty $ render newSearch
               eventLoop tty newSearch
--- Score.hs
+++ Score.hs
@@ -3,26 +3,36 @@ module Score where
 import Data.Char
 import Data.List
 
-data EndMatch = Failure | EndPos Int
-
--- s must start with first char of que
--- query is 
+-- TODO: test this properly
 matchLength :: String -> String -> Int
-matchLength qrest str = go qrest str 0 
-  where 
-    go [] _ lastIndex = lastIndex 
-    go (q:qs) s lastIndex = case elemIndex q s of
-        Just i -> go qs (drop (i + 1) s) (i + 1 + lastIndex)
+matchLength [] _  =  1
+matchLength _ []  =  0
+matchLength (firstChar:rest) str = 
+    case elemIndex firstChar str of
+        Just startPos -> let endPos = endMatch rest (drop (startPos + 1) str) 
+                                                    (startPos + 1)
+                         in if endPos == 0 
+                            then 0
+                            else endPos - startPos
         Nothing -> 0
-    
+  where 
+    endMatch [] _ lastIndex = lastIndex 
+    endMatch (q:qs) s lastIndex = case elemIndex q s of
+        Just i -> endMatch qs (drop (i + 1) s) (i + 1 + lastIndex)
+        Nothing -> 0
+
 
 minMatchLength :: String -> String -> Int
 minMatchLength [] _ = 1
 minMatchLength _ [] = 0
-minMatchLength (first:queryRest) choice = minimum $ filter (>0) matchLengths
-  where
-    firstIndices = elemIndices first choice
-    matchLengths = [1,2]
+minMatchLength query@(q:_) choice = 
+  let lengths = filter (>0) 
+                $ map (matchLength query)
+                $ filter (isPrefixOf [q])
+                $ tails choice
+  in if null lengths 
+     then 0
+     else minimum lengths
 
 score :: String -> String -> Int
 score q choice
