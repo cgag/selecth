63ad01825bd6f3089ff92f712e993f50a6cae393
Curtis Gagliardi <cgag@riseup.net>
2014-05-16 23:26:45 -0700

    Data.Text experiements

--- src/Score.hs
+++ src/Score.hs
@@ -1,47 +1,54 @@
 module Score where
 
-import Data.Char
-import Data.List
+import qualified Data.Text as T
 
 -- TODO: test this properly
+-- pattern match on uncons
 -- TODO: matchLength searches for first char anywhere in string,
 -- it should return 0 if first char isn't correct, otherwise find endMatch
-matchLength :: String -> String -> Int
-matchLength [] _  =  1
-matchLength _ []  =  0
-matchLength (firstChar:rest) str = 
-    case elemIndex firstChar str of
-        Just startPos -> let endPos = endMatch rest (drop (startPos + 1) str) 
-                                                    (startPos + 1)
-                         in if endPos == 0 
-                            then 0
-                            else endPos - startPos
-        Nothing -> 0
-  where 
-    endMatch [] _ lastIndex = lastIndex 
-    endMatch (q:qs) s lastIndex = case elemIndex q s of
-        Just i -> endMatch qs (drop (i + 1) s) (i + 1 + lastIndex)
-        Nothing -> 0
+matchLength :: T.Text -> T.Text -> Int
+matchLength q str
+    | T.null q   = 1
+    | T.null str = 0
+    | otherwise = 
+        let (firstChar, rest) = (T.head q, T.tail q)
+        in
+            case T.findIndex (== firstChar) str of
+                Just startPos -> let endPos = endMatch rest (T.drop (startPos + 1) str) 
+                                                            (startPos + 1)
+                                 in if endPos == 0 
+                                    then 0
+                                    else endPos - startPos
+                Nothing -> 0
+          where 
+            endMatch query s lastIndex 
+                | T.null query = lastIndex
+                | otherwise = let (qFirst, qRest) = (T.head query, T.tail query)
+                              in case T.findIndex (== qFirst) s of
+                                  Just i -> endMatch qRest (T.drop (i + 1) s) (i + 1 + lastIndex)
+                                  Nothing -> 0
 
 
-minMatchLength :: String -> String -> Int
-minMatchLength [] _ = 1
-minMatchLength _ [] = 0
-minMatchLength query@(q:_) choice = 
-  let lengths = filter (>0) 
-                $ map (matchLength query)
-                $ filter (isPrefixOf [q])
-                $ tails choice
-  in if null lengths 
-     then 0
-     else minimum lengths
+minMatchLength :: T.Text -> T.Text -> Int
+minMatchLength query choice
+    | T.null query  = 1
+    | T.null choice = 0
+    | otherwise = let q = T.head query
+                      lengths = filter (>0) 
+                                $ map (matchLength query)
+                                $ filter ((== q) . T.head)
+                                $ filter (not . T.null)
+                                $ T.tails choice
+        in if null lengths 
+           then 0
+           else minimum lengths
 
-score :: String -> String -> Double
+score :: T.Text -> T.Text -> Double
 score q choice
-    | null q      = 1
-    | null choice = 0
-    | otherwise = let minLength = minMatchLength (map toLower q) (map toLower choice)
+    | T.null q      = 1
+    | T.null choice = 0
+    | otherwise = let minLength = minMatchLength (T.toLower q) (T.toLower choice)
                   in if minLength > 0 
-                     then fromIntegral (length q) / fromIntegral minLength -- penalize bigger match lengths
-                          / fromIntegral (length choice) -- penalize longer choice strings
+                     then fromIntegral (T.length q) / fromIntegral minLength -- penalize bigger match lengths
+                          / fromIntegral (T.length choice) -- penalize longer choice strings
                      else 0 
--- test/benchmark/Benchmark.hs
+++ test/benchmark/Benchmark.hs
@@ -1,29 +1,32 @@
-import System.IO
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as TI
 import Criterion.Main
 import Score
 
-scoreBench :: String -> [String] -> Pure
+scoreBench :: T.Text -> [T.Text] -> Pure
 scoreBench query = nf $ map (score query)
 
-buildWord :: String -> Int -> String
-buildWord base baseReps = concat $ replicate baseReps base
+buildWord :: T.Text -> Int -> T.Text
+buildWord base baseReps = T.replicate baseReps base
 
-buildChoices :: String -> Int -> Int -> [String]
+buildChoices :: T.Text -> Int -> Int -> [T.Text]
 buildChoices base baseReps totalChoices = 
     replicate totalChoices $ buildWord base baseReps
 
 main :: IO ()
 main = do
-    contents <- openFile "test/benchmark/words" ReadMode >>= hGetContents 
-    let benchWords = read contents :: [String]
+    contents <- TI.readFile "test/benchmark/words"
+    let benchWords = T.lines contents 
 
     defaultMain [
           bgroup "scoring" 
-            [ bench "non-matching" $ scoreBench (replicate 16 'y') $ buildChoices "x" 16 1000
-            , bench "matching exactly" $ scoreBench (replicate 16 'x') $ buildChoices "x" 16 1000
-            , bench "matching broken up" $ scoreBench (replicate 16 'x') $ buildChoices "xy" 16 1000
-            , bench "overlapping matches" $ scoreBench (replicate 16 'x') $ buildChoices "x" 40 1000
-            , bench "words, non-matching" $ scoreBench (replicate 16 'x') benchWords
+            [ bench "non-matching" $ scoreBench (T.replicate 16 "y") $ buildChoices "x" 16 1000
+            , bench "matching exactly" $ scoreBench (T.replicate 16 "x") $ buildChoices "x" 16 1000
+            , bench "matching broken up" $ scoreBench (T.replicate 16 "x") $ buildChoices "xy" 16 1000
+            , bench "overlapping matches" $ scoreBench (T.replicate 16 "x") $ buildChoices "x" 40 1000
+            , bench "words, non-matching" $ scoreBench (T.replicate 16 "x") benchWords
             , bench "words, matching" $ scoreBench "ungovernableness" benchWords
             ]
          ]
