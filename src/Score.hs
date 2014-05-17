06ebcfa0ea02e7ec7b6672a20717f5da81fe6af4
Curtis Gagliardi <cgag@riseup.net>
2014-05-17 02:13:33 -0700

    vector wip, slower than strings still

--- src/Score.hs
+++ src/Score.hs
@@ -1,20 +1,21 @@
 module Score where
 
-import qualified Data.Text as T
+import qualified Data.Vector as V
+import Data.Char
 
 -- TODO: test this properly
 -- pattern match on uncons
 -- TODO: matchLength searches for first char anywhere in string,
 -- it should return 0 if first char isn't correct, otherwise find endMatch
-matchLength :: T.Text -> T.Text -> Int
+matchLength :: V.Vector Char -> V.Vector Char -> Int
 matchLength q str
-    | T.null q   = 1
-    | T.null str = 0
+    | V.null q   = 1
+    | V.null str = 0
     | otherwise = 
-        let (firstChar, rest) = (T.head q, T.tail q)
+        let (firstChar, rest) = (V.head q, V.tail q)
         in
-            case T.findIndex (== firstChar) str of
-                Just startPos -> let endPos = endMatch rest (T.drop (startPos + 1) str) 
+            case V.findIndex (== firstChar) str of
+                Just startPos -> let endPos = endMatch rest (V.drop (startPos + 1) str) 
                                                             (startPos + 1)
                                  in if endPos == 0 
                                     then 0
@@ -22,33 +23,38 @@ matchLength q str
                 Nothing -> 0
           where 
             endMatch query s lastIndex 
-                | T.null query = lastIndex
-                | otherwise = let (qFirst, qRest) = (T.head query, T.tail query)
-                              in case T.findIndex (== qFirst) s of
-                                  Just i -> endMatch qRest (T.drop (i + 1) s) (i + 1 + lastIndex)
+                | V.null query = lastIndex
+                | otherwise = let (qFirst, qRest) = (V.head query, V.tail query)
+                              in case V.findIndex (== qFirst) s of
+                                  Just i -> endMatch qRest (V.drop (i + 1) s) (i + 1 + lastIndex)
                                   Nothing -> 0
 
+vecTails :: V.Vector a -> [V.Vector a]
+vecTails v 
+    | V.null v = [V.empty]
+    | otherwise = v : vecTails (V.unsafeTail v)
 
-minMatchLength :: T.Text -> T.Text -> Int
+minMatchLength :: V.Vector Char -> V.Vector Char -> Int
 minMatchLength query choice
-    | T.null query  = 1
-    | T.null choice = 0
-    | otherwise = let q = T.head query
-                      lengths = filter (>0) 
-                                $ map (matchLength query)
-                                $ filter ((== q) . T.head)
-                                $ filter (not . T.null)
-                                $ T.tails choice
-        in if null lengths 
+    | V.null query  = 1
+    | V.null choice = 0
+    | otherwise = let q = V.head query
+                      lengths = V.filter (>0) 
+                                $ V.map (matchLength query)
+                                $ V.filter ((== q) . V.head)
+                                $ V.filter (not . V.null)
+                                $ V.fromList
+                                $ vecTails choice
+        in if   V.null lengths 
            then 0
-           else minimum lengths
+           else V.minimum lengths
 
-score :: T.Text -> T.Text -> Double
+score :: V.Vector Char -> V.Vector Char -> Double
 score q choice
-    | T.null q      = 1
-    | T.null choice = 0
-    | otherwise = let minLength = minMatchLength (T.toLower q) (T.toLower choice)
+    | V.null q      = 1
+    | V.null choice = 0
+    | otherwise = let minLength = minMatchLength (V.map toLower q) (V.map toLower choice)
                   in if minLength > 0 
-                     then fromIntegral (T.length q) / fromIntegral minLength -- penalize bigger match lengths
-                          / fromIntegral (T.length choice) -- penalize longer choice strings
+                     then fromIntegral (V.length q) / fromIntegral minLength -- penalize bigger match lengths
+                          / fromIntegral (V.length choice) -- penalize longer choice strings
                      else 0 
--- test/benchmark/Benchmark.hs
+++ test/benchmark/Benchmark.hs
@@ -1,32 +1,29 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-import qualified Data.Text as T
-import qualified Data.Text.IO as TI
+import qualified Data.Vector as V
 import Criterion.Main
 import Score
 
-scoreBench :: T.Text -> [T.Text] -> Pure
+scoreBench :: V.Vector Char -> [V.Vector Char] -> Pure
 scoreBench query = nf $ map (score query)
 
-buildWord :: T.Text -> Int -> T.Text
-buildWord base baseReps = T.replicate baseReps base
+buildWord :: String -> Int -> V.Vector Char
+buildWord base baseReps = V.fromList $ concat $ replicate baseReps base
 
-buildChoices :: T.Text -> Int -> Int -> [T.Text]
+buildChoices :: String -> Int -> Int -> [V.Vector Char]
 buildChoices base baseReps totalChoices = 
     replicate totalChoices $ buildWord base baseReps
 
 main :: IO ()
 main = do
-    contents <- TI.readFile "test/benchmark/words"
-    let benchWords = T.lines contents 
+    contents <- readFile "test/benchmark/words"
+    let benchWords = map V.fromList $ lines contents 
 
     defaultMain [
           bgroup "scoring" 
-            [ bench "non-matching" $ scoreBench (T.replicate 16 "y") $ buildChoices "x" 16 1000
-            , bench "matching exactly" $ scoreBench (T.replicate 16 "x") $ buildChoices "x" 16 1000
-            , bench "matching broken up" $ scoreBench (T.replicate 16 "x") $ buildChoices "xy" 16 1000
-            , bench "overlapping matches" $ scoreBench (T.replicate 16 "x") $ buildChoices "x" 40 1000
-            , bench "words, non-matching" $ scoreBench (T.replicate 16 "x") benchWords
-            , bench "words, matching" $ scoreBench "ungovernableness" benchWords
+            [ bench "non-matching"        $ scoreBench (buildWord "y" 16) $ buildChoices "x" 16 1000
+            , bench "matching exactly"    $ scoreBench (buildWord "x" 16) $ buildChoices "x" 16 1000
+            , bench "matching broken up"  $ scoreBench (buildWord "x" 16) $ buildChoices "xy" 16 1000
+            , bench "overlapping matches" $ scoreBench (buildWord "x" 16) $ buildChoices "x" 40 1000
+            , bench "words, non-matching" $ scoreBench (buildWord "x" 16) benchWords
+            , bench "words, matching"     $ scoreBench (V.fromList "ungovernableness") benchWords
             ]
          ]
