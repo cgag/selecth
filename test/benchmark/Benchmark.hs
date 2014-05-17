362fa42d82a6fa36734701096a496d8e84eab106
Curtis Gagliardi <cgag@riseup.net>
2014-05-17 15:59:44 -0700

    better benchmarking

--- test/benchmark/Benchmark.hs
+++ test/benchmark/Benchmark.hs
@@ -5,25 +5,32 @@ import Score
 scoreBench :: V.Vector Char -> [V.Vector Char] -> Pure
 scoreBench query = nf $ map (score query)
 
-buildWord :: String -> Int -> V.Vector Char
-buildWord base baseReps = V.fromList $ concat $ replicate baseReps base
+buildWord :: Int -> String -> V.Vector Char
+buildWord baseReps base = V.fromList $ concat $ replicate baseReps base
 
 buildChoices :: String -> Int -> Int -> [V.Vector Char]
 buildChoices base baseReps totalChoices = 
-    replicate totalChoices $ buildWord base baseReps
+    replicate totalChoices $ buildWord baseReps base 
 
 main :: IO ()
 main = do
     contents <- readFile "test/benchmark/words"
-    let benchWords = map V.fromList $! lines contents 
+    let benchWords = map V.fromList $ lines contents 
+
+    -- force eval
+    print $ sum $ map V.length benchWords
+
+    let cxs  =  buildChoices "x" 16 1000
+    let cxys = buildChoices "xy" 16 1000
+
 
     defaultMain [
           bgroup "scoring" 
-            [ bench "non-matching"        $ scoreBench (buildWord "y" 16) $ buildChoices "x" 16 1000
-            , bench "matching exactly"    $ scoreBench (buildWord "x" 16) $ buildChoices "x" 16 1000
-            , bench "matching broken up"  $ scoreBench (buildWord "x" 16) $ buildChoices "xy" 16 1000
-            , bench "overlapping matches" $ scoreBench (buildWord "x" 16) $ buildChoices "x" 40 1000
-            , bench "words, non-matching" $ scoreBench (buildWord "x" 16) benchWords
+            [ bench "non-matching" $ scoreBench (buildWord 16 "y")  cxs
+            , bench "matching exactly" $ scoreBench (buildWord 16 "x")  cxs
+            , bench "matching broken up" $ scoreBench (buildWord 16 "x")  cxys
+            , bench "overlapping matches" $ scoreBench (buildWord 16 "x")  cxs
+            , bench "words, non-matching" $ scoreBench (buildWord 16 "x") benchWords
             , bench "words, matching"     $ scoreBench (V.fromList "ungovernableness") benchWords
             ]
          ]
