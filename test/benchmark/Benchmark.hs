dd26474a38fc65f6d8070a60fc0b6935d13a34d3
Curtis Gagliardi <cgag@riseup.net>
2014-05-18 23:46:48 -0700

    Use 1g heap, same performance essentially.

--- test/benchmark/Benchmark.hs
+++ test/benchmark/Benchmark.hs
@@ -16,18 +16,18 @@ main :: IO ()
 main = do
     contents <- openFile "test/benchmark/words" ReadMode >>= hGetContents 
     let benchWords = read contents :: [String]
-    let cxs  =  buildChoices "x" 16 1000
-    let cxys = buildChoices "xy" 16 1000
+    let cxs  =  buildChoices "x"  16 2000
+    let cxys =  buildChoices "xy" 16 2000
 
     -- force eval?
     putStrLn (last benchWords)
 
     defaultMain [
           bgroup "scoring" 
-            [ bench "non-matching" $ scoreBench (replicate 16 'y') $ cxs
-            , bench "matching exactly" $ scoreBench (replicate 16 'x') $ cxs
-            , bench "matching broken up" $ scoreBench (replicate 16 'x') $ cxys
-            , bench "overlapping matches" $ scoreBench (replicate 16 'x') $ cxs
+            [ bench "non-matching" $ scoreBench (replicate 16 'y')  cxs
+            , bench "matching exactly" $ scoreBench (replicate 16 'x')  cxs
+            , bench "matching broken up" $ scoreBench (replicate 16 'x')  cxys
+            , bench "overlapping matches" $ scoreBench (replicate 16 'x')  cxs
             , bench "words, non-matching" $ scoreBench (replicate 16 'x') benchWords
             , bench "words, matching" $ scoreBench "ungovernableness" benchWords
             ]
